47caf2b871e0c6c0ea90fe675b7ef3be5710febe
Abanoub Hany <85456135+abanoubhany216@users.noreply.github.com>
2023-09-03 19:57:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let from = document.getElementById("from");
 let to = document.getElementById("to");
+let sub = document.getElementById("sub");
 let text = document.getElementById("text");
 let result = document.getElementById("result");
-let speak = document.getElementById("speak");
 let speak1 = document.getElementById("speak1");
 let copy = document.getElementById("copy");
 let copy1 = document.getElementById("copy1");
 
 let utter = new SpeechSynthesisUtterance();
-let utter1 = new SpeechSynthesisUtterance();
 
-const languages = ['English','Arabic','French','German',"Italian","Chinese",'Japanese','portuguese']; 
-const codes = ["en","ar","fr","de","it","zh","ja","pt"];
+const languages = ['English','Arabic','French','German',"Italian","Chinese",'Japanese','Portuguese','Russian','Turkish','Spanish']; 
+const codes = ["en","ar","fr","de","it","zh-TW","ja","pt","ru","tr","es"];
+const words = [];
 function checklang(lang) {
     return lang == from.value;
   }
@@ -27,35 +27,35 @@ from.append(option)
 to.append(option1)
 });
 async function translate_fun() {
-translate.engine = "google";
-translate.from = from.value;
-const trans = await translate(text.value,to.value);
-result.innerHTML = trans;
+  let from_code = languages.findIndex(checklang);
+  let to_code = languages.findIndex(checklang1);
+  const url = `https://translation-api4.p.rapidapi.com/translation?from=${codes[from_code]}&to=${codes[to_code]}&query=${text.value}`;
+  const options = {
+      method: 'GET',
+      headers: {
+          'X-RapidAPI-Key': '2485ae7aa5msh68196d82e5384ffp153570jsn1ae24bad1b04',
+          'X-RapidAPI-Host': 'translation-api4.p.rapidapi.com'
+      }
+  };
+  
+  try {
+      const response = await fetch(url, options);
+      const result_ = await response.json();
+      result.value = result_.translation;
+  } catch (error) {
+      console.error(error);
   }
-  text.oninput = ()=>{
-  translate_fun()
-  }
-  from.onchange = ()=>{
-        translate_fun()
-  }
-  to.onchange = ()=>{
-    translate_fun()
-  }
+}
 
-  speak.onclick = ()=>{
-   let speak_code = languages.findIndex(checklang);
-   utter1.lang = codes[speak_code];
-    utter.lang = "en";
-    utter.volume = 2;
-    utter.text = text.value;
-    window.speechSynthesis.speak(utter);
-  }
+sub.onclick = ()=>{
+  translate_fun()
+}
   speak1.onclick = ()=>{
-    let speak_code1 = languages.findIndex(checklang1);
-    utter1.lang = codes[speak_code1];
-    utter1.volume = 2;
-    utter1.text = result.value;
-    window.speechSynthesis.speak(utter1);
+    let speak_code = languages.findIndex(checklang1);
+    utter.lang = codes[speak_code];
+    utter.volume = 2;
+    utter.text = result.value;
+    window.speechSynthesis.speak(utter);
   }
   copy.onclick = ()=>{
     text.select();
